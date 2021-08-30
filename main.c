54933e9da63a7faad02bc3c93f827d4d167c8d94
Mukund Shah <mukundshah058@gmail.com>
2021-08-30 17:20:56 +0545

    Boilerplate

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE HPrevInstance, char* lpCmdLine, int nCmdShow)
+{
+    MessageBox(NULL, "Hello world!", "C GUI", MB_OK);
+    return 0;
+}

57de8af3b6b589526ea5182616aae4cedd9e58bd
Mukund Shah <mukundshah058@gmail.com>
2021-09-06 08:37:37 +0545

    0x01

--- sha256.c
+++ sha256.c
@@ -8,24 +8,15 @@ char *decimalToBinary(int decimal, int bits){
     char *binary = malloc(bits+1);
     if(binary == NULL)
         exit(1);
-    // for (int i = 0; i < bits;i++){
-    //     *binary ='5';
-    //     binary++;
-    // }
-    //binary += bits;
-    //*binary = '\0';
+    
     *(binary+cursor) = '\0';
     
-    //while(decimal > 0){
     while(cursor){
         --cursor;
         *(binary+cursor) = decimal % 2 + '0';
         decimal /= 2;
     }
-    // if(decimal == 0){
-    //     --binary;
-    //     *binary = '0';
-    // }
+
     return binary;
 }
 
@@ -37,42 +28,89 @@ void textToBinary(char *text, int textlength, char *binary, int binaryLength){
         octet = decimalToBinary(*text, 8);
         while(*octet)
             *binary++ = *octet++;
-// *binary++ = ' ';
         ++text;
         octet -= 8;
-
     }
-    //*binary = '\0';
+    *binary = '\0';
     binary -= binaryLength;
     free(octet);
+}
+void bin64(int dec, char *binary, int binaryLength){
+    char *octoctet = malloc(64);
+    if(octoctet == NULL)
+        exit(1);
+    
+        octoctet = decimalToBinary(dec, 64);
+        while(*octoctet)
+            *binary++ = *octoctet++;
+        
+        octoctet -= 64;
+    
+    *binary = '\0';
+    binary -= binaryLength;
+    free(octoctet);
+}
+
+void padMessage(char *msg, char *msg_bin){
+    //char *msg_bin;
+    int msg_len = strlen(msg);
+    int msg_bin_len = msg_len * 8;
+    msg_bin = malloc(msg_bin_len);
+    if (msg_bin == NULL)
+        exit(1);
+
+    textToBinary(msg, msg_len, msg_bin, msg_bin_len);
+    
+
+    msg_bin = realloc(msg_bin, msg_bin_len+1);
+    *(msg_bin + (msg_bin_len)) = '1';
+    *(msg_bin + (msg_bin_len+1)) = '\0';
+    
+    msg_bin_len = strlen(msg_bin);
+    printf("%d\n", msg_bin_len);
+    printf("Your binary encoding is:\n%s\n", msg_bin);
+    int padding = 448 - msg_bin_len % 512;
+    msg_bin = realloc(msg_bin, msg_bin_len+padding);
+    for (int i = msg_bin_len; i < padding; i++)
+    {
+        *(msg_bin + i) = '0';
+    }
+    *(msg_bin + (msg_bin_len+padding)) = '\0';
+    msg_bin_len = strlen(msg_bin);
+    printf("%d\n", msg_bin_len);
+    printf("Your binary encoding is (initial):\n%s\n", msg_bin);
+
+    char *size = malloc(64);
+    size = decimalToBinary(24, 64);
+
+    msg_bin = realloc(msg_bin, msg_bin_len+64);
+    while(*size)
+            *msg_bin++ = *size++;
+    *msg_bin = '\0';
+    // bin64(24, msg_bin, msg_bin_len+padding+64);
+    // *(msg_bin + (msg_bin_len+padding)) = '\0';
+    msg_bin_len = strlen(msg_bin);
+    printf("%d\n", msg_bin_len);
+    printf("Your binary encoding is (last):\n%s", msg_bin);
+    //printf("%s", size);
+    
 }
 
 
 int main(){
-    char *msg_bin,  msg_str[32] = "pa ss\n";
+    char *msg_bin,  msg_str[32] = "abc";
     char ch = 'a';
     
     int msg_len, msg_bin_len;
 
-    // for(int i=0; msg_str[i]!='\0'; i++)
-    //     msg_len++;
     msg_len = strlen(msg_str);
     msg_bin_len = msg_len * 8;
-    msg_bin = malloc(msg_bin_len);
-    if (msg_bin == NULL)
-        return 0;
+    // msg_bin = malloc(msg_bin_len);
+    // if (msg_bin == NULL)
+    //     return 0;
 
-    // textToBinary(msg_str, msg_len, msg_bin, msg_bin_len);
+    //textToBinary(msg_str, msg_len, msg_bin, msg_bin_len);
+    padMessage(msg_str, msg_bin);
+    //printf("Your binary encoding is:\n%s\n", msg_bin);
 
-
-
-
-    // int bin = 0b110;
-    // int ascii = (int)ch;
-    // printf("%d", ascii);
-    // printf("Your binary encoding is:\n%s\n", msg_bin);
-
-    char *out = malloc(8);
-    out = decimalToBinary('a',8);
-    printf("%s", out);
 }
